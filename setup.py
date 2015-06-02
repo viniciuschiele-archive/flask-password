c9fe556620250628289f082f3e27915e285707fe
viniciuschiele <vinicius.chiele@gmail.com>
2015-06-02 08:29:42 +0100

    Added new keywords

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name='flask-password',
     version='0.5.0',
-    packages=['flask_oauth', 'flask-password'],
+    packages=['flask-password'],
     url='https://github.com/viniciuschiele/flask-password',
     license='Apache 2.0',
     author='Vinicius Chiele',
     author_email='vinicius.chiele@gmail.com',
     description='Password hashing for Python 3+',
-    keywords=['flask', 'password'],
+    keywords=['flask', 'password', 'hash', 'hashing'],
     install_requires=['flask==0.10.1'],
     classifiers=[
         'Development Status :: 4 - Beta',
